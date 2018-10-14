29138efe99eb8a566a58861c697e324b7b8aac7f
Guillaume Grossetie <ggrossetie@gmail.com>
2018-10-14 10:30:01 +0200

    Upgrade to Node.js 8+
    - Use Promise for async functions
    - Use standard.js to format code
    - Update README
    - Update LICENSE (copyright 2018)
    - Build against the latest stable and LTS Node.js releases on Travis
    - Update dependencies

--- index.js
+++ index.js
@@ -1,50 +1,56 @@
-'use strict';
-const log = require('bestikk-log');
-const fs = require('fs');
-const path = require('path');
-const child_process = require('child_process');
+'use strict'
+const log = require('bestikk-log')
+const childProcess = require('child_process')
 
 const javaVersionText = function () {
-  const result = child_process.execSync('java -version 2>&1', {encoding: 'utf8'});
-  const lines = result.split('\n');
-  let javaVersionLine;
+  const result = childProcess.execSync('java -version 2>&1', { encoding: 'utf8' })
+  const lines = result.split('\n')
+  let javaVersionLine
   for (let line of lines) {
     if (line.startsWith('java version') || line.startsWith('openjdk version')) {
-      javaVersionLine = line;
+      javaVersionLine = line
     }
   }
   if (javaVersionLine) {
-    var javaVersion = javaVersionLine.match(/"(.*?)"/i)[1];
-    return javaVersion.replace(/\./g, '').replace(/_/g, '');
+    const javaVersion = javaVersionLine.match(/"(.*?)"/i)[1]
+    return javaVersion.replace(/\./g, '').replace(/_/g, '')
   }
-  throw 'Unable to find the java version in: ' + lines; 
-};
+  throw new Error('Unable to find the java version in: ' + lines)
+}
 
 const checkRequirements = function () {
   // Java7 or higher is available in PATH
   try {
     if (javaVersionText() < '170') {
-      log.error('Closure Compiler requires Java7 or higher');
-      return false;
+      log.error('Closure Compiler requires Java7 or higher')
+      return false
     }
   } catch (e) {
-    log.error('error: ' + e);
-    log.error('\'java\' binary is not available in PATH');
-    return false;
+    log.error('error: ' + e)
+    log.error('\'java\' binary is not available in PATH')
+    return false
   }
-  return true;
-};
-
-const Uglify = function() {
-  this.requirementSatisfied = checkRequirements();
+  return true
 }
 
-Uglify.prototype.minify = function (source, destination, callback) {
+const Uglify = function () {
+  this.requirementSatisfied = checkRequirements()
+}
+
+Uglify.prototype.minify = function (source, destination) {
   if (this.requirementSatisfied) {
-    return child_process.exec('java -jar ' + __dirname + '/compiler.jar --warning_level=QUIET --js_output_file=' + destination + ' ' + source, callback);
+    return new Promise((resolve, reject) => {
+      childProcess.exec(`java -jar ${__dirname}/compiler.jar --warning_level=QUIET --js_output_file=${destination} ${source}`, error => {
+        if (error) {
+          log.error('error: ' + error)
+          return reject(error)
+        }
+        return resolve({})
+      })
+    })
   }
-  log.warn('Requirements are not satisfied (see previous errors), skipping minify task');
-  typeof callback === 'function' && callback();
+  log.warn('Requirements are not satisfied (see previous errors), skipping minify task')
+  return Promise.resolve({})
 }
 
-module.exports = new Uglify();
+module.exports = new Uglify()
